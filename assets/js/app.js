4ad430102a436665bca37ab5cc975c306cc74185
Christoph Wittstock <netzhirsch@netzhirsch.de>
2021-03-09 12:04:35 +0100

    Strukturierung

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,4 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
-// import './styles/app.css';
-
-// start the Stimulus application
-// import './bootstrap';
-
 import './../less/global.less';
 import './../less/messages.less';
 
 import './tabs.js';
-// import 'libraries/jquery-ui-1.12.1/jquery-ui.min.css';
-// import 'libraries/jquery-ui-1.12.1/jquery-ui.min.js';
