176a14de803a3f1c4572dff76c74d7c6e62b316f
ASSPICK Versicherungsmakler GmbH <asspick@nn-webdesign.com>
2021-03-15 14:58:46 +0100

    styling

--- src/Controller/DownloadCenter/UserViewController.php
+++ src/Controller/DownloadCenter/UserViewController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\DownloadCenter;
 
 use App\Entity\DownloadCenter\File;
+use App\Entity\DownloadCenter\Folder;
 use App\Repository\DownloadCenter\FileRepository;
 use App\Repository\DownloadCenter\FolderRepository;
 use DateTime;
@@ -14,69 +15,88 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route("/download/center/user")
  */
-class UserViewController extends AbstractController
-{
-    /**
-     * @Route("/view/dummy", name="download_center_user_view_dummy", methods={"GET"})
-     * @return Response
-     */
-    public function userViewDummyAction()
-    {
-        return $this->render('files/index.html.twig');
-    }
+class UserViewController extends AbstractController {
+	/**
+	 * @Route("/view/dummy", name="download_center_user_view_dummy", methods={"GET"})
+	 * @return Response
+	 */
+	public function userViewDummyAction() {
+		return $this->render('files/index.html.twig');
+	}
 
-    /**
-     * @Route("/view/{id}", name="download_center_user_view", methods={"GET"})
-     * @param FolderRepository $folderRepository
-     * @param FileRepository $fileRepository
-     * @param int|null $id
-     * @return Response
-     */
-    public function userViewAction(FolderRepository $folderRepository,FileRepository $fileRepository,int $id = null)
-    {
-        if (empty($id))
-            $folders = $folderRepository->findParentsVisible();
-        else
-            $folders = [$folderRepository->find($id)];
+	/**
+	 * @Route("/view/{id}", name="download_center_user_view", methods={"GET"})
+	 * @param FolderRepository $folderRepository
+	 * @param FileRepository $fileRepository
+	 * @param int|null $id
+	 *
+	 * @return Response
+	 */
+	public function userViewAction(FolderRepository $folderRepository, FileRepository $fileRepository, int $id = null) {
 
-        $today = new DateTime();
+		$today = new DateTime();
 
-        $newFiles = $fileRepository->findNew();
-        foreach ($newFiles as $newFile) {
-            $this->setFileIsNew($today,$newFile->getUpdatedAt(),$newFile);
-        }
+		if (empty($id)) {
+			$folders = $folderRepository->findParentsVisible();
+			$showNewFiles = true;
+			$showBreadcrumbs = false;
+			$breadcrumbs = [];
+		} else {
+			$folder = $folderRepository->find($id);
+			$folders = [$folder];
+			$showBreadcrumbs = !empty($folder->getParent());
+			$showNewFiles = empty($folder->getParent());
+			$breadcrumbs = $this->getBreadcrumbs($folder);
+		}
 
+		$newFiles = $fileRepository->findNew();
+		foreach ($newFiles as $newFile)
+			$this->setFileIsNew($today, $newFile->getUpdatedAt(), $newFile);
 
-        foreach ($folders as $folder) {
-            foreach ($folder->getFiles() as $file) {
-                $this->setFileIsNew($today,$file->getUpdatedAt(),$file);
-            }
-            $children = $folder->getChildren();
-            while($children->count() > 0) {
-                foreach ($children as $child) {
-                    foreach ($child->getFiles() as $childFile) {
-                        $this->setFileIsNew($today,$childFile->getUpdatedAt(),$childFile);
-                    }
-                    $children = $child->getChildren();
-                }
-            }
-        }
+		foreach ($folders as $folder) {
+			foreach ($folder->getFiles() as $file) {
+				$this->setFileIsNew($today, $file->getUpdatedAt(), $file);
+			}
+			$children = $folder->getChildren();
+			while ($children->count() > 0) {
+				foreach ($children as $child) {
+					foreach ($child->getFiles() as $childFile) {
+						$this->setFileIsNew($today, $childFile->getUpdatedAt(), $childFile);
+					}
+					$children = $child->getChildren();
+				}
+			}
+		}
 
-        return $this->render('download_center/user_view/index.html.twig',[
-            'newFiles' => $newFiles,
-            'folders' => $folders,
-            'isChild' => !empty($id)
-        ]);
-    }
+		return $this->render(
+			'download_center/user_view/index.html.twig',
+			[
+				'showNewFiles'    => $showNewFiles,
+				'newFiles'        => $newFiles,
+				'showBreadcrumbs' => $showBreadcrumbs,
+				'breadcrumbs'     => $breadcrumbs,
+				'folders'         => $folders,
+				'isChild'         => !empty($id),
+			]
+		);
+	}
 
-    private function setFileIsNew(DateTime $today, DateTimeInterface $updatedAt,File $file){
-        if ($this->isUpdatedAWeekAgo($today, $updatedAt)) {
-            $file->setIsNew(true);
-        }
-    }
-    private function isUpdatedAWeekAgo(DateTime $today, DateTimeInterface $updatedAt): bool
-    {
-        $daysBetween = $today->diff($updatedAt)->days;
-        return $daysBetween !== false && $today->diff($updatedAt)->days < 7;
-    }
+	private function setFileIsNew(DateTime $today, DateTimeInterface $updatedAt, File $file) {
+		if ($this->isUpdatedAWeekAgo($today, $updatedAt)) {
+			$file->setIsNew(true);
+		}
+	}
+
+	private function isUpdatedAWeekAgo(DateTime $today, DateTimeInterface $updatedAt):bool {
+		$daysBetween = $today->diff($updatedAt)->days;
+		return $daysBetween !== false && $today->diff($updatedAt)->days < 7;
+	}
+
+	private function getBreadcrumbs(Folder $folder):array {
+		do {
+			$result[] = clone $folder;
+			$folder = $folder->getParent();
+		} while (!empty($folder));
+		return array_reverse($result);
+	}
 }
