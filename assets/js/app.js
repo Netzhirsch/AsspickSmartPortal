f4123532e258d6d02ed77afee0658f7a050f78b2
luhmann <luhmann@netzhirsch.de>
2021-04-22 07:40:28 +0200

    MUPS-30 Fontawesome lokal einbinden

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,7 @@ import './../less/global.less';
 import './../less/messages.less';
 
 import './tabs.js';
+import '@fortawesome/fontawesome-pro/js/fontawesome';
+import '@fortawesome/fontawesome-pro/js/solid';
+import '@fortawesome/fontawesome-pro/js/regular';
+import '@fortawesome/fontawesome-pro/js/brands';
