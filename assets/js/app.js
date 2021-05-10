6f66e3d1d774e1c66c09c1fee18702dc21bd2977
Christoph Wittstock <netzhirsch@netzhirsch.de>
2021-05-10 12:35:47 +0200

    Diverses

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,5 +2,12 @@ import './../less/global.less';
 import './../less/messages.less';
 
 import './tabs.js';
-import '@fortawesome/fontawesome-pro/js/all';
+//import '@fortawesome/fontawesome-pro/js/all';
 import '@fortawesome/fontawesome-pro/css/all.min.css';
+
+$('#topbar .toggle-nav').click(function() {
+	$('#mobile-navigation').addClass('visible');
+});
+$('#mobile-navigation .close').click(function() {
+	$('#mobile-navigation').removeClass('visible');
+});
--- src/Form/Filter/UserViewFilterType.php
+++ src/Form/Filter/UserViewFilterType.php
@@ -8,23 +8,28 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserViewFilterType extends AbstractType
-{
+class UserViewFilterType extends AbstractType {
 
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('name',TextType::class,[
-                'label' => 'Name',
-                'required' => false
-            ])
-        ;
-    }
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$builder
+			->add(
+				'name',
+				TextType::class,
+				[
+					'label'    => 'Name',
+					'required' => false,
+					'attr'     => [
+						'placeholder' => 'Suchbegriff eingeben ...',
+					],
+				]
+			);
+	}
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => UserViewFilter::class,
-        ]);
-    }
+	public function configureOptions(OptionsResolver $resolver) {
+		$resolver->setDefaults(
+			[
+				'data_class' => UserViewFilter::class,
+			]
+		);
+	}
 }
