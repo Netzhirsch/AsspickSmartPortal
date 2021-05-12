7364dedc00e2af31ba162b85eda0914202d6262b
luhmann <luhmann@netzhirsch.de>
2021-05-12 16:03:46 +0200

    MUPS-35 Mailversand

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -100,7 +100,7 @@ class RegistrationController extends AbstractController
         $messageAdmin = 'Jemand hat versucht sich mit der E-Mail Adresse: '.$emailAddressUser;
 
         if (!empty($code))
-            $messageAdmin .= ' und dem Code:'.$code;
+            $messageAdmin .= ' und dem Code: '.$code;
         $messageAdmin .= ' zu registrieren.'.PHP_EOL.'Bitte aktivieren Sie gegebenenfalls den Benutzer.';
 
         $subjectAdmin = 'Neue Registrierung';
@@ -110,7 +110,7 @@ class RegistrationController extends AbstractController
 
             $messageAdmin = 'Jemand hat sich mit der E-Mail Adresse: '.$emailAddressUser;
             if (!empty($code))
-                $messageAdmin .= ' und dem Code '.$code;
+                $messageAdmin .= ' und dem Code: '.$code;
             $messageAdmin .= ' registriert.';
 
             $messageUser .= PHP_EOL.'Sie können sich nun einloggen.';
